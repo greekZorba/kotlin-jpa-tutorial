31981e03a7619d477c2465ed783349fb00596a03
greekZorba <smiling.jinhak@gmail.com>
2020-02-28 22:59:41 +0900

    init setting

---
+++ src/main/kotlin/com/jpa/TutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.jpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<TutorialApplication>(*args)
+}
